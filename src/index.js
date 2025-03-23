e40215842f726e19821b2efa668293381b6359dd
Dhaif Jaber -Y El Shaabi <51718255+djabber2019@users.noreply.github.com>
2025-03-23 21:04:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './global.css'; // Import global styles
+//import './global.css'; // Import global styles
 
 // Error Boundary Component
 class ErrorBoundary extends React.Component {
