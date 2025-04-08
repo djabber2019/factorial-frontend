78e3692b317a2aeaa3db7cd56cff6df1a304fc7a
Dhaif Jaber -Y El Shaabi <51718255+djabber2019@users.noreply.github.com>
2025-04-08 22:05:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -194,7 +194,7 @@ const script = document.createElement('script');
     if (!paypalSdkReady || !paypalButtonContainerRef.current) return;
 
     try {
-      window.paypal_sdk.Buttons({
+      window.paypal.Buttons({
         style: {
           layout: 'vertical',
           color: 'blue',
