7478421610c8b3d2ec839b4784ed34da29e77c0c
Dhaif Jaber -Y El Shaabi <51718255+djabber2019@users.noreply.github.com>
2025-04-11 16:39:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const API_BASE = window.location.protocol === 'https:'
      ? 'https://factorial-backend.sliplane.app'
      : 'http://localhost:8000';
 const PAYPAL_CLIENT_ID = "Aee8X8eVx-SCFL1sOeACxyozvwhpzNFKzQlbZbzAviLrB3YGkHUR_z1YFQYyg1i6iC2ultefi-BXHnPW";
-const HOSTED_BUTTON_ID = "SPTGWEPQYGYEE"; // Corrected button ID
+const HOSTED_BUTTON_ID = "9EUNPRHJB3SNQ"; // Corrected button ID
 const PAYMENT_THRESHOLD = process.env.REACT_APP_PAYMENT_THRESHOLD || 1000;
 const PAYMENT_AMOUNT_USD = process.env.REACT_APP_PAYMENT_AMOUNT_USD || 4.99;
 
